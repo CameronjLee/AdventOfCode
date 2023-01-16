5cf829f620a506b8fd65dd2e05b31dca7ea19d1c
JBANORTHWEST\CameronLee <cameron.lee@jbaconsulting.com>
2023-01-16 14:48:03 +0000

    Add solution for day 4

---
+++ AdventOfCode4/AdventOfCode4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AdventOfCode4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"../../../../input/AOC4 input.txt";
+            int containedWithinPair = 0;
+
+            // Reads file and parses to int. Checks whether ints are any ints are contained within eachother
+            foreach (string line in File.ReadLines(inputPath))
+            {
+                char[] delimeters = { '-', ',' };
+                string[] elfAreaStrings = line.Split(delimeters);
+                List<int> elfAreaInts = new List<int>();
+                for (int i = 0; i < elfAreaStrings.Length; i++)
+                {
+                    elfAreaInts.Add(Int32.Parse(elfAreaStrings[i]));
+                }
+
+                if (elfAreaInts[2] <= elfAreaInts[0] && elfAreaInts[0] <= elfAreaInts[3])
+                {
+                    containedWithinPair++;
+                }
+                else if (elfAreaInts[2] <= elfAreaInts[1] && elfAreaInts[1] <= elfAreaInts[3])
+                {
+                    containedWithinPair++;
+                }
+                else if (elfAreaInts[0] <= elfAreaInts[2] && elfAreaInts[2] <= elfAreaInts[1])
+                {
+                    containedWithinPair++;
+                }
+
+                //Console.WriteLine(containedWithinPair);
+            }
+        }
+    }
+}
---
+++ AdventOfCode4/AdventOfCode4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace AdventOfCode4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"../../../../input/AOC4 input.txt";
+            int containedWithinPair = 0;
+
+            //Reads file and parses to int. Then compares the first side of the ints to the second to see if they are all contained
+            foreach (string line in File.ReadLines(inputPath))
+            {
+                char[] delimeters = { '-', ',' };
+                string[] elfAreaStrings = line.Split(delimeters);
+                List<int> elfAreaInts = new List<int>();
+                for (int i = 0; i < elfAreaStrings.Length; i++)
+                {
+                    elfAreaInts.Add(Int32.Parse(elfAreaStrings[i]));
+                }
+
+                if (elfAreaInts[0] <= elfAreaInts[2] && elfAreaInts[1] >= elfAreaInts[3])
+                {
+                    containedWithinPair++;
+                }
+                else if (elfAreaInts[0] >= elfAreaInts[2] && elfAreaInts[1] <= elfAreaInts[3])
+                {
+                    containedWithinPair++;
+                }
+                else if (elfAreaInts[0] == elfAreaInts[2] && elfAreaInts[1] == elfAreaInts[3]) 
+                {
+                    containedWithinPair++;
+                }
+                else
+                {
+                    continue;
+                }
+
+                Console.WriteLine(containedWithinPair);
+            }
+        }
+    }
+}

07f4b4caf9aae4d6289483f15850e84ed035e88d
JBANORTHWEST\CameronLee <cameron.lee@jbaconsulting.com>
2023-01-16 12:35:45 +0000

    Add solution for day 3

---
+++ AdventOfCode3/AdventOfCode3.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"../../../../Input/AOC3 input.txt";
+            int totalPriorityOfItems = 0;
+            char badgeItem = 'A';
+            int priorityOfBadge = 0;
+            List<string> groupOfElves = new List<string>();
+
+            // Loops through lines in fil and appends in list then finds values of repeated chars
+            foreach (string line in File.ReadLines(inputPath))
+            {
+                groupOfElves.Add(line);
+                Console.WriteLine(groupOfElves.Count);
+                if (groupOfElves.Count == 3)
+                {
+                    foreach (char item in groupOfElves[0])
+                    {
+                        if(groupOfElves[1].Contains(char.ToString(item)) && groupOfElves[2].Contains(char.ToString(item)))
+                        {
+                            badgeItem = item;
+                        }
+                    }
+                    groupOfElves.Clear();
+
+                    if (char.IsLower(badgeItem))
+                    {
+                        priorityOfBadge = badgeItem - 96;
+                    }
+                    else if (char.IsUpper(badgeItem))
+                    {
+                        priorityOfBadge = badgeItem - 38;
+                    }
+                    totalPriorityOfItems += priorityOfBadge;
+                }
+
+                //Console.WriteLine(badgeItem);
+                //Console.WriteLine(priorityOfBadge);
+                //Console.WriteLine(totalPriorityOfItems);
+            }
+        }
+    }
+}
---
+++ AdventOfCode3/AdventOfCode3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"../../../../Input/AOC3 input.txt"))
+            {
+                int totalPriorityOfItems = 0;
+                string fileLine;
+
+                // Loops through lines and compares strings to find repeated values and priority
+                while ((fileLine = sr.ReadLine()) != null)
+                {
+                    int compartmentContentsLength = (fileLine.Length)/2;
+                    string compartment1 = fileLine.Substring(0, compartmentContentsLength);
+                    string compartment2 = fileLine.Substring(compartmentContentsLength, compartmentContentsLength);
+                    char duplicateItem = 'A';
+                    int priorityOfItem = 0;
+                    
+                    foreach (char item in compartment1)
+                    {
+                        foreach (char item2 in compartment2)
+                        {
+                            if (item == item2)
+                            {
+                                duplicateItem = item;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (char.IsLower(duplicateItem))
+                    {
+                        priorityOfItem = duplicateItem - 96;
+                    }
+                    else if (char.IsUpper(duplicateItem))
+                    {
+                        priorityOfItem = duplicateItem - 38;
+                    }
+
+                    totalPriorityOfItems += priorityOfItem;
+
+                    //Console.WriteLine(duplicateItem);
+                    //Console.WriteLine(priorityOfItem);
+                    //Console.WriteLine(totalPriorityOfItems);
+                }
+            }
+        }
+    }
+}
